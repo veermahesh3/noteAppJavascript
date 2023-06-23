4983c09ff83d0c98df7555c6c4ec3393a210f8a3
veermahesh3 <48675547+veermahesh3@users.noreply.github.com>
2023-06-23 05:23:10 -0700

    Add files via upload
    note app using javascript

---
+++ index.js
@@ -0,0 +1,39 @@
+const notesContainer=document.querySelector(".notes-container");
+const createBtn=document.querySelector(".btn");
+let notes=document.querySelectorAll(".input-box");
+
+function showNotes(){
+    notesContainer.innerHTML=localStorage.getItem("notes");
+}
+showNotes();
+function updateStorage(){
+    localStorage.setItem("notes",notesContainer.innerHTML);
+}
+
+
+
+createBtn.addEventListener('click',()=>{
+    let inputBox=document.createElement("p");
+    let img=document.createElement("img");
+    inputBox.className="input-box";
+    inputBox.setAttribute("contenteditable","true");
+    img.src="https://cdn-icons-png.flaticon.com/512/3405/3405244.png";
+    notesContainer.appendChild(inputBox).appendChild(img);
+    updateStorage();
+})
+
+notesContainer.addEventListener('click',function(e){
+   
+    if(e.target.tagName==="IMG"){
+        e.target.parentElement.remove();
+        updateStorage();
+    }else if(e.target.tagName=="p"){
+        notes=document.querySelectorAll(".input-box");
+        notes.forEach(nt=>{
+            nt.onkeyup=function(){
+                updateStorage();
+            }
+        })
+    
+}
+})
